24107f494319c28ea76f6045417547d495123fe9
Janasicherdoch <scholzeja@gmail.com>
2020-04-15 11:30:15 +0200

    start over and added Martins import function and how to turn import data into numpy array

--- AdventofCode_03.py
+++ AdventofCode_03.py
@@ -1,32 +1,30 @@
+#this code takes a list of claims to a piece fabric
+# such as #1 @ 1,3: 4x4 (first claim: 1 inch from left and 3 inches from the top, size: width=4 inches, height = 4 inches)
+# and determins on how many square inches two or more claims overlap
+
 import numpy as np
 
-fabrics = np.genfromtxt("C:/Users/schol/PyCharm Projekte/AdventofCode/AdventofCode_03_fabrics.txt",
-                        dtype = None,
-                        comments = None,
-                        delimiter =(' ') )
+with open("AdventofCode_03_fabrics.txt", "r") as f:
+    #strip the newline character
+    fabrics = list(line[:-1] for line in f.readlines())
+#print(fabrics)
 
+#split one claim into 5 integers (Danke Martin!)
+def split_claim_into_int(claim):
+    id_claim, rest = claim.split("@")
+    id_claim = int(id_claim.lstrip("#"))
+    offset, rest = rest.split(":")
+    x0, y0 = offset.split(",")
+    x0 = int(x0)
+    y0 = int(y0)
+    width, height = rest.split("x")
+    width = int(width)
+    height = int(height)
+    return(id_claim, x0, y0, width, height)
 
-def create_int_array(input_array):
-    new_array = []
-    for n in range(len(input_array)):
-      element = input_array[n]
-      element_2 = element[2]
-      element_3 = element[3]
-      index = n
-      pos_x = element_2[:3]
-      pos_y = element_2[4:7]
-      size_x = element_3[:2]
-      size_y = element_3[3:]
-      new_element = [index, pos_x, pos_y, size_x, size_y]
-      new_array.append(new_element)
-    return new_array
+#create numpy array from integer claims
+fabrics_int=np.array(list(split_claim_into_int(claims) for claims in fabrics))
 
-fabrics_int = np.array(create_int_array(fabrics))
 print(fabrics_int)
-print(fabrics_int[0, :])
-fabrics_size = np.size(fabrics_int)
-fabrics_size_y= np.size(fabrics_int,0)
-fabrics_size_x= np.size(fabrics_int,1)
-print(fabrics_size, fabrics_size_y, fabrics_size_x)
 
-print(int(fabrics_int[0,1])+int(fabrics_int[0,2]))
+
